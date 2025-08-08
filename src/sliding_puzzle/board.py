82dbc0acc6b26b40323ee929d513b8dc6a6ae3c3
Jonathan Pereda <jpered4@wgu.edu>
2025-08-08 04:41:54 -0700

    Added check if 3x3 grid is solvable using inversion count on flat list

--- src/sliding_puzzle/board.py
+++ src/sliding_puzzle/board.py
@@ -8,7 +8,7 @@ class Board:
         #Create the flat list of numbers based on board size including an empty space: None
         flat_list = list(range(1, self.size**2)) + [None]
 
-        #Compile our flat list into a matrix 
+        #Compile our flat list into a grid 
         rows = []
         for start in range(0, len(flat_list), self.size):
             rows.append(flat_list[start:start+self.size])
@@ -26,3 +26,36 @@ class Board:
                 else:
                     clean_list.append(" ")
             print(" | ".join(clean_list))
+
+    def flatten_grid(self):
+        flat_list = []
+        #Flatten grid into a single list ignoring None
+        for row in self.tiles:
+            for val in row:
+                if val is not None:
+                    flat_list.append(val)
+        return flat_list
+    
+    def count_inversions(self, flat):
+        count = 0
+        #Check every number and see if all numbers after it are smaller than it
+        for i in range(0,len(flat)-1):
+            for j in range(i+1,len(flat)):
+                if flat[i] > flat [j]:
+                    count = count + 1
+        return count
+    
+    def is_solvable(self):
+        #Pull the current grids inversion count
+        flat = self.flatten_grid()
+        inversions = self.count_inversions(flat)
+        #Check solvability based on grid size
+        if self.size == 3:
+            return inversions % 2 == 0
+        elif self.size == 4:
+            return False
+        else:
+            return False
+
+
+
--- src/sliding_puzzle/cli.py
+++ src/sliding_puzzle/cli.py
@@ -3,3 +3,5 @@ from .board import Board
 def start_game():
     board = Board(3)
     board.display()
+    #flat = [4, 6, 8, 3, 1, 2, 7, 5]
+    print(board.is_solvable())
